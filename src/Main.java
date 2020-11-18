f6cac89c3ef0b17b41dd19b9378d71434e38ca88
Tatyana Hripkova <tkhripkova@bk.ru>
2020-11-18 22:22:34 +0200

    Four commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class giMain {
+    public static void main(String[] args) {
+        int balanc = 2_000_000_000;
+        int tranfer =500_000_000;
+        int total = balanc+tranfer;
+        System.out.println(total);
+    }
+}
+
